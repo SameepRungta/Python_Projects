e51c8f714862f417fa50c08953e8503c73e44720
SameepRungta <sameeprungta2002@gmail.com>
2023-12-28 12:07:17 +0530

    Day 5

--- 2.IO.py
+++ 2.IO.py
@@ -1,3 +1,3 @@
 # input("What is your name")
 
-print("Hello "+input("What is your name"))
+print("Hello "+input("What is your name?"))
---
+++ 20.Height.py
@@ -0,0 +1,4 @@
+height= input("Input a list of height ").split()
+for n in range(0, len(height)):
+    height[n]= (height[n])
+print(height)
